b2f9c96fa5321d59fd3d767ca5f56b0a04c59680
EJIMVINCENT <EJIMOVC@GMAIL.COM>
2023-09-18 01:54:45 -0700

    saving changes before moving to test branch 18/sep || 09:54

--- _printf.c
+++ _printf.c
@@ -93,11 +93,10 @@ int *_printBuffer(const char *string, int count[2])
 			i++;
 			index++;
 
-			if (i == BUFFER_SIZE)
+			if (i >= BUFFER_SIZE)
 			{
 				printed += i; 
 				write(1, buffer, i);
-		
 				i = 0;
 			}
 		}
